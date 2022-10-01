1bb1829c42002da9338c3ba24daaf59c099b8e9c
mrfrac <fracturer@gmail.com>
2022-10-01 12:55:39 +0500

    Промежуточные наработки по главной странице

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-(function(){})();
+(function(){
+  const button = document.querySelector(".main-nav__toggle");
+  const mainNav = document.querySelector(".main-nav");
+
+  button.addEventListener("click", () => {
+    if (mainNav.classList.contains("main-nav--opened")) {
+      mainNav.classList.remove("main-nav--opened")
+      mainNav.classList.add("main-nav--closed");
+    } else {
+      mainNav.classList.remove("main-nav--closed")
+      mainNav.classList.add("main-nav--opened");
+    }
+  });
+})();
